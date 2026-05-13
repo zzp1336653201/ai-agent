fb487c6635560d7fc72e70a88f03fbe646cd3d6e
zzp <1336653201@qq.com>
2026-05-13 22:55:07 +0800

    fix: MockRepository 方法签名与 service 接口匹配

--- cmd/server/mock.go
+++ cmd/server/mock.go
@@ -26,6 +26,7 @@ func (r *MockWorkflowRepository) List(status string, page, size int) ([]*model.W
 }
 func (r *MockWorkflowRepository) Update(w *model.Workflow) error                          { return nil }
 func (r *MockWorkflowRepository) Delete(id string) error                                 { return nil }
+func (r *MockWorkflowRepository) GetWorkflowForExecution(id string) (*model.Workflow, error) { return nil, nil }
 func (r *MockWorkflowRepository) SaveExecution(exec *model.WorkflowExecution) error       { return nil }
 func (r *MockWorkflowRepository) GetExecution(id string) (*model.WorkflowExecution, error) { return nil, nil }
 func (r *MockWorkflowRepository) ListExecutions(workflowID string, status string, page, size int) ([]*model.WorkflowExecution, int64, error) {
@@ -37,7 +38,9 @@ type MockDocumentRepository struct{}
 
 func (r *MockDocumentRepository) Create(doc *model.Document) error             { return nil }
 func (r *MockDocumentRepository) GetByID(id string) (*model.Document, error)   { return nil, nil }
-func (r *MockDocumentRepository) List() ([]*model.Document, error)             { return []*model.Document{}, nil }
+func (r *MockDocumentRepository) List(page, size int) ([]*model.Document, int64, error) {
+	return []*model.Document{}, 0, nil
+}
 func (r *MockDocumentRepository) Delete(id string) error                    { return nil }
 
 // 确保实现了 service 接口
